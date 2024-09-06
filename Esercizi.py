a16787a56808d582e2cf9d3cb9689ba246751aa1
FrancescoVecchiato <francescovecchiato528@gmail.com>
2024-09-06 13:02:55 +0200

    Add files via upload

---
+++ Esercizi.py
@@ -0,0 +1,74 @@
+nome = "Francesco"
+
+print(nome)
+
+eta = 21
+
+print(eta)
+
+pi = 3.14159
+
+print(pi)
+
+lunghezza = 9
+lunghezza = 15
+
+print(lunghezza)
+
+nome_completo = "Francesco Vecchiato"
+
+print(nome_completo)
+
+eta_futura = eta + 10
+
+print(eta_futura)
+
+cognome = "Vecchiato"
+anno_di_nascita = 2003
+
+print(nome, cognome, anno_di_nascita)
+
+nome = "Gianni"
+cognome = "Verdi"
+anno_di_nascita = 1989
+
+print(nome, cognome, anno_di_nascita)
+
+eta_attuale = 2024 - 2003
+
+print(eta_attuale)
+
+numero1 = 20
+numero2 = 12
+somma = numero1 + numero2
+
+print(somma)
+
+differenza = numero1 - numero2
+
+print(differenza)
+
+prodotto = numero1 * numero2
+
+print(prodotto)
+
+quoziente = numero1 / numero2
+
+print(quoziente)
+
+resto = numero1 % numero2
+
+print(resto)
+
+numero1 += 1
+numero2 -= 3
+
+print(numero1, numero2)
+
+risultato = (numero1 * (numero1 + 5)) + (numero2 ** 2)
+
+print(risultato)
+
+numero1 /= numero1
+
+print(numero1)
